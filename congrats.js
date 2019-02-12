07f9cfd9ae45f153f87208dff698a3e7a24578c5
primekhan555 <primekhan555@gmail.com>
2019-02-12 09:11:51 +0500

    added word third

--- congrats.js
+++ congrats.js
@@ -1,2 +1,2 @@
-console.log('congrats to the first position holder of the class');
+console.log('congrats to the first & third position holder of the class');
 console.log('congrats to the second position holder of the class');
